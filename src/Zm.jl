d1d5f506e6c777012bb854ed86821a4993cc49a9
casv2 <casv2@cam.ac.uk>
2020-09-14 14:40:29 +0100

    type E_tot fix

--- src/Zm.jl
+++ src/Zm.jl
@@ -44,7 +44,7 @@ function Zmethod(IP, at, nsteps, dt, A, N, file; write_at=false)
             T[i] = Ek / (1.5 * kB)
             P[i] = -tr(stress(IP, at))/3.0
             if i % 10 == 0
-                write(io, "$(Ep) $(Ek) $(Etot[i]) $(T[i]) $(P[i]) \n")
+                write(io, "$(Ep) $(Ek) $(E_tot[i]) $(T[i]) $(P[i]) \n")
             end
 
             v = at.P ./ m
